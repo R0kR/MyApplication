7c7dd057551531a693684e42b0200f0693fd4aca
Bappa Das <bappad@amazon.com>
2018-03-17 18:33:18 +0530

    Mongo DB test

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,17 @@
-
+import com.mongodb.MongoClient;
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hi Team! Let's start the mission guys");
+        
+        		MongoClient client = new MongoClient("ds115579.mlab.com",15579);
+        		String connectPoint = client.getConnectPoint();
+        		System.out.println(connectPoint);
+        		client.close();
+        	
+         
+        
 	}
 
 }
